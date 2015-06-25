2f1d509c48b2a260fe6f7b8e466bfc56704fad8e
Enamul Hassan <enamsustcse@gmail.com>
2015-06-25 20:11:58 +0600

    License update

--- src/judgecpp/RunInCmd.java
+++ src/judgecpp/RunInCmd.java
@@ -1,10 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 Enamul.
+ *
+ * Most of my softwares are open for educational purpose, but some are 
+ * confidential. So, before using it openly drop me some lines at
+ *
+ *      enamsustcse@gmail.com
+ *
+ * I do not guarantee that the software would work properly. There could
+ * remain bugs. If you found any of them, kindly report me.
+ * If you need to use this or some part of it, use it at your own risk.
+ * This software is not a professionally developed, so commercial use 
+ * is not approved by default.
  */
 
-package judge;
+package judgecpp;
 
 import java.io.IOException;
 
